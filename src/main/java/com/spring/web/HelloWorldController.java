3bf51dc479b22c4ed2f4161c956f787e7e0233b7
ratneshranj <ratneshranj@gmail.com>
2022-08-20 00:15:11 +0530

    Basic Spring Boot Actuator

---
+++ src/main/java/com/spring/web/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.spring.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(value = "/hello")
+	public String sayHello() {
+		return "Hello, Welcome to course on Microservices";
+	}
+
+}
---
+++ src/test/java/com/spring/actuator/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.actuator.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
